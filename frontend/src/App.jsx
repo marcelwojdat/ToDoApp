11331f86cf8f0426a4008aa08255c4af405b494d
MarcelWojdat <marceliwojdat@gmail.com>
2026-03-28 10:36:41 +0100

    Add simple task-adding form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const [newTaskTitle, setNewTaskTitle] = useState('')
 
   const fetchTasks = () => {
     fetch('http://127.0.0.1:8000/api/tasks/')
@@ -9,6 +10,29 @@ function App() {
       .then(data => setTasks(data))      
       .catch(error => console.error('Error:', error))
   }
+  const addTask = (e) => {
+    e.preventDefault()
+
+    if (!newTaskTitle) return
+
+    fetch('http://127.0.0.1:8000/api/tasks/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: newTaskTitle,
+        completed: false
+      }),
+    })
+    .then(response => response.json())
+    .then(() => {
+      setNewTaskTitle('')
+      fetchTasks()
+    })
+  }
+
+
 
   useEffect(() => {
     fetchTasks()
@@ -17,6 +41,10 @@ function App() {
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
       <h1>My tasks</h1>
+      <form onSubmit={addTask}>  
+        <input type="text" value={newTaskTitle} onChange={(e) => setNewTaskTitle(e.target.value)} placeholder='Enter task title '/>
+        <button type='submit'>Add</button>
+      </form>
       <ul>
         {tasks.map(task => (
           <li key={task.id}>
